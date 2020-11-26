ffb5948d7f3bc90f0e65b204f68e5b3f40b2aaf9
hamkasatria <muhhamkasatrianto@gmail.com>
2020-11-26 09:01:20 +0700

    menambahkan upload dan get file

--- src/main/java/com/project/perpustakaan/PerpustakaanApplication.java
+++ src/main/java/com/project/perpustakaan/PerpustakaanApplication.java
@@ -1,13 +1,27 @@
 package com.project.perpustakaan;
 
+import javax.annotation.Resource;
+
+import com.project.perpustakaan.service.FilesStorageService;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan({"com.project"})
-public class PerpustakaanApplication {
+public class PerpustakaanApplication implements CommandLineRunner {
+	@Resource
+    FilesStorageService storageService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PerpustakaanApplication.class, args);
 	}
+
+	@Override
+    public void run(String... arg) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
+    }
 }
